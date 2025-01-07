1eadfa04b2ea190ee83ce77bbb17bba75db61cc3
Shunsuke Tanizaki <shun21github>
2025-01-07 21:25:20 +0900

    Fix Header design

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import { Link, useI18next, useTranslation } from 'gatsby-plugin-react-i18next';
+// import queryString from 'query-string';
+import { useLocation } from '@reach/router';
 
 const Header = () => {
     const { languages, originalPath } = useI18next();
     const { t } = useTranslation();
+    const location = useLocation();
+
+    const checkLang = (lng) => {
+        let url = location.pathname.substring(1, 3);
+
+        switch (url) {
+            case 'ja':
+                break;
+            case 'en':
+                break;
+            default:
+                url = 'cs';
+        }
+
+        const result = lng == url ? true : false;
+        return result;
+    };
 
     return (
         <div className="header">
@@ -14,12 +33,14 @@ const Header = () => {
                 {/* <Link to="/menu">{t('Menu')}</Link> */}
                 {/* <Link to="/takeaway">{t('Takeaway')}</Link> */}
             </div>
-            <ul className="header__links--languages">
+            <ul className="header__languages">
                 {languages.map((lng) => (
                     <li key={lng}>
-                        <Link to={originalPath} language={lng}>
-                            {lng}
-                        </Link>
+                        {!checkLang(lng) ? (
+                            <Link to={originalPath} language={lng}>
+                                {lng}
+                            </Link>
+                        ) : null}
                     </li>
                 ))}
             </ul>
